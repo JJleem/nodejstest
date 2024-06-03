00b013d97e31c40c6df74d07b39fede08eac9ce7
leemjaejun <leemjaejun@gmail.com>
2024-06-03 16:03:16 +0900

    240603

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import express from "express"
+
+// const express = require("express");
+
+// const app = express();
+
+// app.use();
+
+// 2024년 => 다양한 엔지니어들이 저마다의 모듈을 사용하더라도 여튼 현시점에서 가장 최신 문법으로 변현된 타입의 문법을 사용하는 것이 가장 적합한 선택!
+
+console.log("How are you?");
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.map((i) => i * i));
